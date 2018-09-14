974f715aa50f50579b5cd4d79fc161de68e48f18
antonstrand <jag@antonstrand.se>
2018-09-14 16:53:10 +0200

    Adding comments on methods

--- model/Username.php
+++ model/Username.php
@@ -7,12 +7,27 @@ class Username
     private const MIN_LENGTH = 3;
     private $username;
 
+    /**
+     * Create a new Username
+     *
+     * @throws Exception "Username is missing" if the username is empty
+     * @throws Exception "Username has too few characters, at least 3 characters." if the username is too short
+     * @param string $username
+     */
     public function __construct(string $username)
     {
         $this->setUsername($username);
     }
 
-    public function setUsername(string $username)
+    /**
+     * Update username
+     *
+     * @throws Exception "Username is missing" if the username is empty
+     * @throws Exception "Username has too few characters, at least 3 characters." if the username is too short
+     * @param string $username
+     * @return void
+     */
+    public function setUsername(string $username): void
     {
         $username = $this->clean($username);
 
@@ -20,24 +35,42 @@ class Username
             throw new \Exception("Username is missing");
         }
 
-        if ($this->isToShort($username)) {
+        if ($this->isTooShort($username)) {
             throw new \Exception("Username has too few characters, at least " . self::MIN_LENGTH . " characters.");
         }
 
         $this->username = $username;
     }
 
-    private function clean($username): string
+    /**
+     * Remove eventual tags and whitespece.
+     *
+     * @param string $username
+     * @return string
+     */
+    private function clean(string $username): string
     {
         return trim(strip_tags($username));
     }
 
+    /**
+     * Checks if the username is empty
+     *
+     * @param string $username
+     * @return boolean
+     */
     private function isEmpty(string $username): bool
     {
         return empty($username);
     }
 
-    private function isToShort(string $username): bool
+    /**
+     * Checks if the username is too short
+     *
+     * @param string $username
+     * @return boolean
+     */
+    private function isTooShort(string $username): bool
     {
         return strlen($username) < self::MIN_LENGTH;
     }
