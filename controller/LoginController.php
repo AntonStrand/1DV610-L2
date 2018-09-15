db840443028c0943e4d4480d148d7372c9fee60b
antonstrand <jag@antonstrand.se>
2018-09-15 21:37:42 +0200

    Handling logout

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -5,26 +5,38 @@ class LoginController
 {
     private $view;
     private $db;
-    private $isLoggedIn = false;
 
     public function __construct(\View\LoginView $loginView)
     {
-        $isLoggedIn = false;
         $this->view = $loginView;
         $this->db = new \Model\Database();
         $this->handleSubmit();
+        $this->handleLogout();
     }
 
     public function isLoggedIn(): bool
     {
-        return $this->isLoggedIn;
+        return \Session::getVariable(\Session::IS_LOGGED_IN) || false;
     }
 
     private function handleSubmit(): void
     {
         if ($this->view->shouldSubmit()) {
             $formData = $this->view->getFormData();
-            $this->isLoggedIn = $this->db->authenticateUser($formData["username"], $formData["password"]);
+            $isLoggedIn = $this->isLoggedIn() || $this->db->authenticateUser($formData["username"], $formData["password"]);
+
+            if ($isLoggedIn) {
+                \Session::setVariable(\Session::IS_LOGGED_IN, true);
+            } else {
+                \Session::setVariable(\Session::IS_LOGGED_IN, false);
+            }
+        }
+    }
+
+    private function handleLogout(): void
+    {
+        if ($this->view->shouldLogout()) {
+            \Session::setVariable(\Session::IS_LOGGED_IN, false);
         }
     }
 
