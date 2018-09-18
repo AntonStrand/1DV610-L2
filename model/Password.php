a588c85ed64b0f2a51f08ee8c73387d45abb688f
antonstrand <jag@antonstrand.se>
2018-09-18 21:12:04 +0200

    Re-implementing user models

---
+++ model/Password.php
@@ -0,0 +1,60 @@
+<?php
+namespace Model;
+
+class Password
+{
+    private const MIN_LENGTH = 6;
+    private $password;
+
+    /**
+     * Create a new Password
+     *
+     * @throws Exception "Password is missing" if the $password is empty
+     * @throws Exception "Password has too few characters, at least 6 characters." if the $password is too short
+     * @param string $password
+     */
+    public function __construct(string $password)
+    {
+        $this->setPassword($password);
+    }
+    /**
+     * Update password
+     *
+     * @throws Exception "Password is missing" if the $password is empty
+     * @throws Exception "Password has too few characters, at least 6 characters." if the $password is too short
+     * @param string $password
+     * @return void
+     */
+
+    public function setPassword(string $password)
+    {
+        if (empty($password)) {
+            throw new \Exception("Password is missing");
+        }
+        if ($this->isTooShort($password)) {
+            throw new \LengthException("Password has too few characters, at least " . self::MIN_LENGTH . " characters.");
+        }
+        $this->password = $password;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * Checks if the password is too short
+     *
+     * @param string $password
+     * @return boolean
+     */
+    private function isTooShort(string $password): bool
+    {
+        return strlen($password) < self::MIN_LENGTH;
+    }
+}
---
+++ model/UserCredentials.php
@@ -0,0 +1,23 @@
+<?php
+namespace model;
+
+class UserCredentials
+{
+    private $username;
+    private $password;
+    private $keepLoggedIn;
+    /**
+     * Create a instance of a UserCredentials
+     *
+     * @throws \Exception
+     * @param string $username
+     * @param string $password
+     * @param boolean $keepLoggedIn
+     */
+    public function __construct(string $username, string $password, bool $keepLoggedIn)
+    {
+        $this->username = new Username($username);
+        $this->password = new Password($password);
+        $this->keepLoggedIn = $keepLoggedIn;
+    }
+}
