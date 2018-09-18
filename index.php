c909d134a8b73bc564ec2ce03d470d55dd804d5f
antonstrand <jag@antonstrand.se>
2018-09-18 21:13:32 +0200

    Adding dependencies

--- index.php
+++ index.php
@@ -1,15 +1,26 @@
 <?php
 
-//INCLUDE THE FILES NEEDED...
+# View
 require_once 'view/LoginView.php';
 require_once 'view/DateTimeView.php';
 require_once 'view/LayoutView.php';
 
+# Controller
+require_once 'controller/LoginController.php';
+
+# Model
+require_once 'model/Username.php';
+require_once 'model/Password.php';
+require_once 'model/UserCredentials.php';
+
 //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
+
 //CREATE OBJECTS OF THE VIEWS
 $v = new \view\LoginView();
 $dtv = new \view\DateTimeView();
 $lv = new \view\LayoutView();
 $lv->render(false, $v, $dtv);
+
+$c = new \controller\LoginController($v);
