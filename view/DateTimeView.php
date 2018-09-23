0ec836fb5f65d90acb82726cb48337e36a118edd
antonstrand <jag@antonstrand.se>
2018-09-23 13:08:25 +0200

    Using " instead of '

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -9,14 +9,14 @@ class DateTimeView
     {
         date_default_timezone_set("Europe/Stockholm");
 
-        $weekday = date('l');
-        $dayOfMonth = date('jS');
-        $month = date('F');
-        $year = date('Y');
+        $weekday = date("l");
+        $dayOfMonth = date("jS");
+        $month = date("F");
+        $year = date("Y");
         $timeOfDay = date("H:i:s");
 
-        $timeString = $weekday . ', the ' . $dayOfMonth . ' of ' . $month . ' ' . $year . ', The time is ' . $timeOfDay;
+        $timeString = $weekday . ", the " . $dayOfMonth . " of " . $month . " " . $year . ", The time is " . $timeOfDay;
 
-        return '<p>' . $timeString . '</p>';
+        return "<p>" . $timeString . "</p>";
     }
 }
