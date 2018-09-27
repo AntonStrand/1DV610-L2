5bec3a7f87f226ce2ca9619d985e0f33b01a5534
antonstrand <jag@antonstrand.se>
2018-09-27 14:54:56 +0200

    Render an error page incase something goes wrong

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ require_once 'view/RegisterView.php';
 require_once 'view/LoginView.php';
 require_once 'view/DateTimeView.php';
 require_once 'view/LayoutView.php';
+require_once 'view/Error500.php';
 
 # Controller
 require_once 'controller/LoginController.php';
@@ -23,12 +24,12 @@ require_once 'model/Storage.php';
 require_once 'model/Session.php';
 require_once 'model/Database.php';
 
-//MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
 ini_set('session.use_only_cookies', true);
 ini_set('session.use_trans_sid', false);
 
-// TODO: Add try catch
-$main = new \controller\MainController();
-$main->route();
+try {
+    $main = new \controller\MainController();
+    $main->route();
+} catch (\Exception $e) {
+    new \view\Error500();
+}
---
+++ view/Error500.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace view;
+
+class Error500
+{
+    public function __construct()
+    {
+        echo $this->getHTML();
+    }
+
+    private function getHTML(): string
+    {
+        return '<!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <title>Login Example</title>
+        </head>
+        <body>
+          <h1>500</h1>
+          <p>Sorry, something went wrong. Please try again later.</p>
+         </body>
+      </html>
+    ';
+    }
+}
