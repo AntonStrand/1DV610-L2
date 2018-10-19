1dc3e201c1f70008df2ecfcf799ab8572dd64b75
antonstrand <jag@antonstrand.se>
2018-10-19 17:19:09 +0200

    Reordering the class

--- view/LoginView.php
+++ view/LoginView.php
@@ -25,6 +25,15 @@ class LoginView implements IView
         $this->state = $state;
     }
 
+    public function response()
+    {
+        $message = $this->getMessage();
+
+        return $this->state->isAuthenticated()
+        ? $this->generateLogoutButtonHTML($message)
+        : $this->generateLoginFormHTML($message);
+    }
+
     public function shouldLogin(): bool
     {
         return $this->hasClickedLogin() && $this->isInputValid() && !$this->state->isAuthenticated();
@@ -32,18 +41,18 @@ class LoginView implements IView
 
     public function shouldLogout(): bool
     {
-        return $this->hasClickedLogout();
+        return isset($_POST[self::$logout]);
     }
 
     public function getUserCredentials(): UserCredentials
     {
-        if ($this->hasRequiredInput()) {
-            $keepLoggedIn = $this->keepLoggedIn();
-            $username = $this->getRequestUsername();
-            $password = $this->getRequestPassword();
+        assert($this->hasRequiredInput());
 
-            return new UserCredentials($username, $password, $keepLoggedIn);
-        }
+        $keepLoggedIn = $this->keepLoggedIn();
+        $username = $this->getRequestUsername();
+        $password = $this->getRequestPassword();
+
+        return new UserCredentials($username, $password, $keepLoggedIn);
     }
 
     public function shouldSaveCookie(): bool
@@ -54,26 +63,6 @@ class LoginView implements IView
     public function shouldLoginByCookie(): bool
     {
         return !$this->state->isAuthenticated() && $this->isCookieSet();
-    }
-
-    public function removeCookie(): void
-    {
-        $expires = time() - 100;
-        \setcookie(self::$cookieName, null, $expires, "/", "", "", true);
-        \setcookie(self::$cookiePassword, null, $expires, "/", "", "", true);
-
-        // To remove the cookies during this request.
-        $_COOKIE[self::$cookieName] = null;
-        $_COOKIE[self::$cookiePassword] = null;
-    }
-
-    public function getCookieData(): UserCredentials
-    {
-        if ($this->isCookieSet()) {
-            $username = $_COOKIE[self::$cookieName];
-            $password = $_COOKIE[self::$cookiePassword];
-            return new UserCredentials($username, $password);
-        }
     }
 
     public function useDefaultWelcomeMessage(): void
@@ -103,27 +92,6 @@ class LoginView implements IView
         $this->message = "Bye bye!";
     }
 
-    /**
-     * Create HTTP response
-     *
-     * Should be called after a login attempt has been determined
-     *
-     * @return string and writes to cookies!
-     */
-    public function response()
-    {
-        $message = $this->getMessage();
-
-        return $this->state->isAuthenticated()
-        ? $this->generateLogoutButtonHTML($message)
-        : $this->generateLoginFormHTML($message);
-    }
-
-    private function isCookieSet(): bool
-    {
-        return isset($_COOKIE[self::$cookieName]) && isset($_COOKIE[self::$cookiePassword]);
-    }
-
     public function setCookie(): void
     {
         $aDayInSeconds = 86400;
@@ -138,45 +106,34 @@ class LoginView implements IView
         $_COOKIE[self::$cookiePassword] = $password;
     }
 
-    /**
-     * Generate HTML code on the output buffer for the logout button
-     * @param $message, String output message
-     * @return void, BUT writes to standard output!
-     */
-    private function generateLogoutButtonHTML($message)
+    public function removeCookie(): void
     {
-        return '
-			<form  method="post" >
-				<p id="' . self::$messageId . '">' . $message . '</p>
-				<input type="submit" name="' . self::$logout . '" value="logout"/>
-			</form>
-		';
+        $expires = time() - 100;
+        \setcookie(self::$cookieName, null, $expires, "/", "", "", true);
+        \setcookie(self::$cookiePassword, null, $expires, "/", "", "", true);
+
+        // To remove the cookies during this request.
+        $_COOKIE[self::$cookieName] = null;
+        $_COOKIE[self::$cookiePassword] = null;
     }
 
-    /**
-     * Generate HTML code on the output buffer for the logout button
-     * @param $message, String output message
-     * @return string
-     */
-    private function generateLoginFormHTML($message)
+    public function getCookieData(): UserCredentials
     {
-        return '
-			<form method="post" >
-				<fieldset>
-					<legend>Login - enter Username and password</legend>
-					<p id="' . self::$messageId . '">' . $message . '</p>
+        if ($this->isCookieSet()) {
+            $username = $_COOKIE[self::$cookieName];
+            $password = $_COOKIE[self::$cookiePassword];
+            return new UserCredentials($username, $password);
+        }
+    }
 
-					<label for="' . self::$name . '">Username :</label>
-					<input type="text" id="' . self::$name . '" name="' . self::$name . '" value="' . $this->getUsername() . '" />
-					<label for="' . self::$password . '">Password :</label>
-					<input type="password" id="' . self::$password . '" name="' . self::$password . '" />
-					<label for="' . self::$keep . '">Keep me logged in  :</label>
-					<input type="checkbox" id="' . self::$keep . '" name="' . self::$keep . '" />
+    private function isCookieSet(): bool
+    {
+        return isset($_COOKIE[self::$cookieName]) && isset($_COOKIE[self::$cookiePassword]);
+    }
 
-					<input type="submit" name="' . self::$login . '" value="login" />
-				</fieldset>
-			</form>
-		';
+    private function isNewMember(): bool
+    {
+        return !$this->state->isAuthenticated() && $this->state->hasUsername();
     }
 
     private function getMessage(): string
@@ -185,7 +142,7 @@ class LoginView implements IView
             return $this->message;
         }
 
-        if (!$this->state->isAuthenticated() && $this->state->hasUsername()) {
+        if ($this->isNewMember()) {
             return "Registered new user.";
         }
 
@@ -217,16 +174,48 @@ class LoginView implements IView
 
     private function isInputValid(): bool
     {
-        if ($this->hasRequiredInput()) {
-            try {
-                $this->getUserCredentials();
-                return true;
-            } catch (Exception $e) {
-                return false;
-            }
+        if (!$this->hasRequiredInput()) {
+            return false;
         }
 
-        return false;
+        try {
+            $this->getUserCredentials();
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
+
+    }
+
+    private function generateLogoutButtonHTML($message)
+    {
+        return '
+			<form  method="post" >
+				<p id="' . self::$messageId . '">' . $message . '</p>
+				<input type="submit" name="' . self::$logout . '" value="logout"/>
+			</form>
+		';
+    }
+
+    private function generateLoginFormHTML($message)
+    {
+        return '
+			<form method="post" >
+				<fieldset>
+					<legend>Login - enter Username and password</legend>
+					<p id="' . self::$messageId . '">' . $message . '</p>
+
+					<label for="' . self::$name . '">Username :</label>
+					<input type="text" id="' . self::$name . '" name="' . self::$name . '" value="' . $this->getUsername() . '" />
+					<label for="' . self::$password . '">Password :</label>
+					<input type="password" id="' . self::$password . '" name="' . self::$password . '" />
+					<label for="' . self::$keep . '">Keep me logged in  :</label>
+					<input type="checkbox" id="' . self::$keep . '" name="' . self::$keep . '" />
+
+					<input type="submit" name="' . self::$login . '" value="login" />
+				</fieldset>
+			</form>
+		';
     }
 
     private function cleanInput(string $input): string
@@ -242,12 +231,14 @@ class LoginView implements IView
 
     private function hasUsername(): bool
     {
-        return isset($_POST[self::$name]) && !empty($this->cleanInput($this->getRequestUsername()));
+        return isset($_POST[self::$name]) &&
+        !empty($this->cleanInput($this->getRequestUsername()));
     }
 
     private function hasPassword(): bool
     {
-        return isset($_POST[self::$password]) && !empty($this->cleanInput($this->getRequestPassword()));
+        return isset($_POST[self::$password]) &&
+        !empty($this->cleanInput($this->getRequestPassword()));
     }
 
     private function getRequestUsername(): string
@@ -274,20 +265,10 @@ class LoginView implements IView
         return isset($_POST[self::$login]);
     }
 
-    private function hasClickedLogout(): bool
-    {
-        return isset($_POST[self::$logout]);
-    }
-
     private function getUsername(): string
     {
         return $this->state->hasUsername()
         ? $this->state->getUsername()
         : $this->getRequestUsername();
-    }
-
-    private function loginFailed(): bool
-    {
-        return strlen($this->getFormError()) == 0 && !$this->state->isAuthenticated();
     }
 }
