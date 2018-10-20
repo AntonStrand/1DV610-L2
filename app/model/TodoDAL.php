e19e2ea06e85cbe29d8feeb37bf74fdfd3eaf325
antonstrand <jag@antonstrand.se>
2018-10-20 23:09:50 +0200

    Save todo

---
+++ app/model/TodoDAL.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\model;
+
+class TodoDAL
+{
+    private static $TABLE_NAME = "todos";
+    private $db;
+
+    public function __construct(Database $db)
+    {
+        $this->db = $db;
+    }
+
+    public function save(Todo $todo): void
+    {
+        $this->db->connect();
+
+        $username = $todo->getUsername();
+        $task = $todo->getTask();
+        $isComplete = intval($todo->isComplete());
+
+        $stmt = $this->$db->prepareStatement(
+            "INSERT INTO " . self::$TABLE_NAME . " (username, task, isComplete) VALUES (?, ?, ?);"
+        );
+
+        $stmt->bind_param("ssi", $username, $task, $isComplete);
+        $stmt->execute();
+        $this->db->disconnect();
+    }
+}
