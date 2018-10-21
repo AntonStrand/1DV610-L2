40227d9b76e6dcd4882cc4bad8a1e82bf5be1cdb
antonstrand <jag@antonstrand.se>
2018-10-21 20:22:03 +0200

    Prepare for release

--- index.php
+++ index.php
@@ -7,12 +7,8 @@ require_once 'Settings.php';
 ini_set('session.use_only_cookies', true);
 ini_set('session.use_trans_sid', false);
 
-//MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
-
-// try {
-new \app\controller\AppController();
-// } catch (\Exception $e) {
-//     new \app\view\Error500();
-// }
+try {
+    new \app\controller\AppController();
+} catch (\Exception $e) {
+    new \app\view\Error500();
+}
