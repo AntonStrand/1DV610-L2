6c40b3fe0e7bec0ed7c50bc9ce2519f734052c28
antonstrand <jag@antonstrand.se>
2018-10-21 09:37:41 +0200

    Receving id to indentify the todo later

--- app/model/Todo.php
+++ app/model/Todo.php
@@ -7,12 +7,14 @@ class Todo
     private $username;
     private $task;
     private $isComplete;
+    private $id;
 
-    public function __construct(string $username, string $task, bool $isComplete)
+    public function __construct(string $username, string $task, bool $isComplete, int $id = 0)
     {
         $this->username = $username;
         $this->task = $task;
         $this->isComplete = $isComplete;
+        $this->id = $id;
     }
 
     public function getUsername(): string
