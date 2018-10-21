97fa9c8c291c780e76829005c5d7ff25efecf255
antonstrand <jag@antonstrand.se>
2018-10-21 11:03:51 +0200

    Get a users todos

--- app/model/TodoDAL.php
+++ app/model/TodoDAL.php
@@ -28,4 +28,26 @@ class TodoDAL
         $stmt->execute();
         $this->db->disconnect();
     }
+
+    public function getTodosForUser(string $username): array
+    {
+        $this->db->connect();
+
+        $stmt = $this->db->prepareStatement(
+            "SELECT * FROM " . self::$TABLE_NAME . " WHERE username=?;"
+        );
+
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $rows = $stmt->get_result();
+
+        $todos = array();
+
+        foreach ($rows as $row) {
+            $todos[] = new Todo($row["username"], $row["task"], $row["isComplete"], $row["id"]);
+        }
+
+        $this->db->disconnect();
+        return $todos;
+    }
 }
